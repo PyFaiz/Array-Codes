937e308e932bd82d6c9fa1a4e165ecd274e45690
PyFaiz <116499683+PyFaiz@users.noreply.github.com>
2022-10-23 19:26:50 +0530

    Add files via upload

---
+++ Basic_Array_Printing.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int arr[5] = { 1,2,3,4,9 };
+	cout << "Printing Array ";
+	for (int i = 0; i < 5; i++) {
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+}
