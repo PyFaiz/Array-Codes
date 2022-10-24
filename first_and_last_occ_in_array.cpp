ec5f8da4806069131e8907450f3789a88e2d1f43
PyFaiz <116499683+PyFaiz@users.noreply.github.com>
2022-10-24 16:23:24 +0530

    Create first_and_last_occ_in_array.cpp

---
+++ first_and_last_occ_in_array.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+int firstOcc(int arr[], int size, int item) {
+	int start = 0;
+	int end = size - 1;
+	int mid = start + (end - start) / 2;
+	int ans = -1;
+	while (start <= end) {
+		if (arr[mid] == item) {
+			ans = mid;
+			end = mid - 1;
+		}
+		else if (arr[mid] > item) {
+			end = mid - 1;
+		}
+		else if (arr[mid] < item) {
+			start = mid + 1;
+		}
+		mid = start + (end - start) / 2;
+	}
+	return ans;
+}
+
+int lastOcc(int arr[], int size, int item) {
+	int start = 0;
+	int end = size - 1;
+	int mid = start + (end - start) / 2;
+	int ans = -1;
+	while (start <= end) {
+		if (arr[mid] == item) {
+			ans = mid;
+			start = mid + 1;
+		}
+		else if (item > arr[mid]) {
+			start = mid + 1;
+		}
+		else if (item < arr[mid]) {
+			end = mid - 1;
+		}
+		mid = start + (end - start) / 2;
+	}
+	return ans;
+}
+
+
+
+int main() {
+	int arr[7] = { 1,2,3,5,5,5,7 };
+	cout << "First Occurance is :" << firstOcc(arr, 7, 5);
+	cout << "\nLast Occurance is :" << lastOcc(arr, 7, 5);
+
+}
