a9b4fcf10a21718805a1829804d03f20fa33951c
PyFaiz <116499683+PyFaiz@users.noreply.github.com>
2023-05-04 01:38:25 +0530

    Add files via upload

---
+++ basic_linked_list.java
@@ -0,0 +1,19 @@
+public class basic_linked_list {
+    public static class Node{
+        int data;
+        Node next;
+        Node(int data){
+            this.data = data;
+        }
+    }
+    public static void main(String[] args){
+        Node n1 = new Node(2);
+        Node n2 = new Node(5);
+        Node n3 = new Node(7);
+        Node n4 = new Node(9);
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+        System.out.println(n3.next.data);
+    }
+}
