0fe64f08e89cbb2576d17f72f93b521bc97c26aa
PyFaiz <mohd.faiz.ansari.csiot21@ggits.net>
2023-10-10 22:56:05 +0530

    hehe

---
+++ longest_subarray_sum.java
@@ -0,0 +1,33 @@
+public class longest_subarray_sum {
+    public static void main(String[] args) {
+        int[] array = {2,3,5,1,9};
+        System.out.println(longest_sum(array, 10));
+    }
+
+    public static int longest_sum(int[] array,int k){
+        int count = 0;
+        int max = 0;
+        int sum = 0;
+        for(int i=0;i<array.length;i++){
+            sum = 0;
+            count = 0;
+            sum += array[i];
+            count++;
+            for(int j=i+1;j<array.length;j++){
+                sum += array[j];
+                count++;
+                if(sum == k){
+                    if(max<=count){
+                        max = count;
+                    }
+                }
+            }
+        }
+
+
+
+        return max;
+
+    }
+
+}
