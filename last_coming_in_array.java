d0b641e2a166d6e27dec3b0ce481cb5290f3e749
PyFaiz <mohd.faiz.ansari.csiot21@ggits.net>
2023-10-12 23:53:02 +0530

    last

---
+++ last_coming_in_array.java
@@ -0,0 +1,12 @@
+public class last_coming_in_array {
+    public static void main(String[] args) {
+        
+    }
+
+    public static int last_occ(int[] array,int x)
+    {
+        int last = -1;
+        return last;
+    }
+
+}
