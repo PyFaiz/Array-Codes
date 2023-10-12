5ff1403ff917ebc56e42ccc85ec3292ba94726ec
PyFaiz <mohd.faiz.ansari.csiot21@ggits.net>
2023-10-12 23:29:57 +0530

    2 done

---
+++ search_insert_position.java
@@ -0,0 +1,33 @@
+public class search_insert_position {
+    public static void main(String[] args) {
+        int[] arra = {1,2,4,7};
+        System.out.println(inserted(arra, 2));
+    }
+
+    public static int inserted(int[] arra,int x)
+    {
+        int n = arra.length;
+        int start = 0;
+        int current_smol = n;
+        int end = n;
+        int mid = (start+end)/2;
+        while(start<=end)
+        {
+            mid = (start+end)/2;
+            if(arra[mid]>x)
+            {       
+                current_smol = mid;
+                end = mid-1;
+            }
+            else if(arra[mid]<x)
+            {
+                start = mid+1;
+            }
+            else if(arra[mid] == x){
+                return 1;
+            }
+        }
+        return current_smol;
+    }
+}
+
---
+++ upper_bound_expert.java
@@ -0,0 +1,25 @@
+public class upper_bound_expert {
+    public static void main(String[] args) {
+        
+    }
+
+    public static int find_upper_bound(int[] array,int x)
+    {
+        int end = array.length;
+        int start = 0;
+        int current_big = end;
+        while(start<=end)
+        {
+            int mid = (start + end)/2;
+            if(array[mid] <= x){
+                current_big = mid;
+                start = mid +1; 
+            }
+            else if(array[mid]>x){
+                end = mid - 1;
+            }
+        }
+        return current_big;
+
+    }
+}
