e47fb458212d13dd3d573495cdc88d93c2d70884
PyFaiz <mohd.faiz.ansari.csiot21@ggits.net>
2023-11-02 01:40:00 +0530

    head and tail

--- doubly_linked_list.java
+++ doubly_linked_list.java
@@ -7,6 +7,33 @@ public class doubly_linked_list {
             this.data = data;
         }
     }
+
+    public static void display(Node head){
+        Node temp  = head;
+        while(temp!=null){
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+
+    public static void insertattail(Node head,int data){
+        Node temp = head;
+        while(temp.next != null){
+            temp  = temp.next;
+        }
+        Node t = new Node(data);
+        temp.next = t;
+        t.previous = temp;
+    }
+
+    public static Node insertatbegin(int data,Node head){
+        Node temp = new Node(data);
+        temp.previous  = null;
+        temp.next = head;
+        head.previous = temp;
+        head = temp;
+        return temp;
+    }
     public static void main(String[] args) {
         Node a = new Node(3);
         Node b = new Node(4);
@@ -20,11 +47,8 @@ public class doubly_linked_list {
         c.next = d;
         d.previous  = c;
         d.next = null;
-        Node temp =  a;
-        while(temp!=null){
-            System.out.print(temp.data + " ");
-            temp = temp.next;
-        }
+        insertattail(a, 90);
+        display(a);
 
 
     }   
